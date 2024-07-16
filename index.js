b357c2b2375e08ef601e68682d25efd4d29af81a
CoconutCannon <morteapollyon@gmail.com>
2024-07-16 15:47:23 +0300

    Commit

---
+++ index.js
@@ -0,0 +1,62 @@
+import express from 'express'
+const app = express()
+
+const cors = require('cors')
+app.use(cors())
+
+
+let notes = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.statusMessage = "Skidibi sigma rizz sigma mewing";
+    response.status(404).end()
+  }
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    const info = `Phonebook has info for ${notes.length} people`
+    response.send(`<p>${info}<br> ${date}</p>`)
+  })
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
